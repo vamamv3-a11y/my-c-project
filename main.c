c7e1243594cc32b2a9141c1b63a5f67e34025e16
vamamv3-a11y <vamamv3@gmail.com>
2026-05-01 15:29:06 +0300

    main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "include/lab2.h"
 #include "include/lab2(add).h"
 #include "include/lab3.h"
+#include "include/lab3(add).h"
+#include "include/lab4.h"
 #include "include/lab4(add).h"
 #include "include/lab5.h"
 #include "include/lab5(add).h"
@@ -19,9 +21,11 @@ int main(void)
         printf("3.  Лаба 2\n");
         printf("4.  Лаба 2 доп\n");
         printf("5.  Лаба 3\n");
-        printf("6.  Лаба 4 доп\n");
-        printf("7.  Лаба 5\n");
-        printf("8.  Лаба 5 доп\n");
+        printf("6.  Лаба 3 доп\n");
+        printf("7.  Лаба 4\n");
+        printf("8.  Лаба 4 доп\n");
+        printf("9.  Лаба 5\n");
+        printf("10. Лаба 5 доп\n");
         printf("0.  Выход\n");
         printf("Выбор: ");
         if (scanf("%d", &choice) != 1)
@@ -36,9 +40,11 @@ int main(void)
             case 3:  run_lab2();      break;
             case 4:  run_lab2_add();  break;
             case 5:  run_lab3();      break;
-            case 6:  run_lab4_add();  break;
-            case 7:  run_lab5();      break;
-            case 8:  run_lab5_add();  break;
+            case 6:  run_lab3_add();  break;
+            case 7:  run_lab4();      break;
+            case 8:  run_lab4_add();  break;
+            case 9:  run_lab5();      break;
+            case 10: run_lab5_add();  break;
             case 0:  printf("Выход.\n"); return 0;
             default: printf("Неверный выбор.\n");
         }
